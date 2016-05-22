2485dde5121be58cd3a6e8a2c3a92297fc0a8bd1
Darius Bacon <darius@wry.me>
2016-05-22 13:08:28 -0400

    strip commas

--- absyntax.py
+++ absyntax.py
@@ -9,17 +9,17 @@ Box = Struct('box', 'name stmts')
 Decl = Struct('decl', 'names')
 
 Conn = Struct('conn', 'points')
-Put = Struct('put', 'opt_name, box')
+Put = Struct('put', 'opt_name box')
 
 Equate = Struct('=', 'parts')
 Default = Struct('~', 'parts')
 
-Add = Struct('+', 'arg1, arg2')
-Sub = Struct('-', 'arg1, arg2')
-Mul = Struct('*', 'arg1, arg2')
-Div = Struct('/', 'arg1, arg2')
+Add = Struct('+', 'arg1 arg2')
+Sub = Struct('-', 'arg1 arg2')
+Mul = Struct('*', 'arg1 arg2')
+Div = Struct('/', 'arg1 arg2')
 
 Ref = Struct('ref', 'name')
-Of = Struct('of', 'ref, field')
+Of = Struct('of', 'ref field')
 
 Literal = Struct('literal', 'n')
