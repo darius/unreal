88bf2777000929b91b832c3bb5699ca0dc40185a
Darius Bacon <darius@wry.me>
2016-05-29 23:04:01 -0400

    handle forward refs

--- interpreter.py
+++ interpreter.py
@@ -10,16 +10,29 @@ import linear_constraints as LC
 import linear_equations as LE
 import drawing
 
-class Environment(Struct('types drawers frames')):
+class Environment(Struct('types constrainers drawers frames')):
     def spawn(self, frame):
-        return Environment(self.types, self.drawers, self.frames + (frame,))
+        return Environment(self.types, self.constrainers, self.drawers,
+                           self.frames + (frame,))
+    def add_constrainer(self, constrainer):
+        self.constrainers.append((constrainer, self))
+    def add_drawer(self, drawer):
+        self.drawers.append((drawer, self))
 
 def run(defs):
-    env = Environment({box.name: box for box in defs}, [], ({},))
-    env.types['main'].make(env)
+    root_env = Environment({box.name: box for box in defs},
+                           [], [], ({},))
+    # First we create all the variables...
+    root_env.types['main'].make(root_env)
+
+    # ...then create the constraints. We waited because an equation
+    # might use forward refs.
+    for constrainer, env in root_env.constrainers:
+        constrainer.constrain(env)
+
     drawing.begin()
-    for drawer, subenv in env.drawers:
-        drawer.draw(subenv)
+    for drawer, env in root_env.drawers:
+        drawer.draw(env)
     drawing.end()
 
 counter = count(1)
@@ -30,24 +43,24 @@ def gensym():
 class Box(Struct('name stmts')):
     def make(self, env):
         for stmt in self.stmts:
-            stmt.run(env)
+            stmt.build(env)
 
 class Decl(Struct('names')):
-    def run(self, env):
+    def build(self, env):
         for name in self.names:
             lin_exp = LE.LinExp(0, [(LC.Variable(name), 1)])
             env.frames[-1][name] = LC.Number(lin_exp)
 
 class Conn(Struct('points')):
-    def run(self, env):
-        env.drawers.append((self, env))
+    def build(self, env):
+        env.add_drawer(self)
     def draw(self, env):
         points = [p.evaluate(env).get_value() for p in self.points]
         drawing.polyline(map(to_coords, points))
 
 class Text(Struct('justified string where')):
-    def run(self, env):
-        env.drawers.append((self, env))
+    def build(self, env):
+        env.add_drawer(self)
     def draw(self, env):
         at = self.where.evaluate(env).get_value()
         drawing.text(self.string, self.justified or 'center', to_coords(at))
@@ -56,20 +69,22 @@ def to_coords(point):
     return point.real, point.imag
 
 class Put(Struct('opt_name box')):
-    def run(self, env):
+    def build(self, env):
         name = self.opt_name or gensym()  # (The default name's just for debugging.)
         subenv = env.spawn({})
         env.types[self.box.name].make(subenv)
         env.frames[-1][name] = subenv.frames[-1]
         for stmt in self.box.stmts:
-            stmt.run(subenv)
+            stmt.build(subenv)
 
 class Default(Struct('parts')):
-    def run(self, env):
+    def build(self, env):
         assert False, "XXX unimplemented"
 
 class Equate(Struct('parts')):
-    def run(self, env):
+    def build(self, env):
+        env.add_constrainer(self)
+    def constrain(self, env):
         reduce(LC.equate, (expr.evaluate(env) for expr in self.parts))
 
 class Ref(Struct('name')):
