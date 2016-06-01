c7170b32fb61f85a03fbf8611b7be2a28b81ec2c
Darius Bacon <darius@wry.me>
2016-06-01 00:23:06 -0400

    rewrite the solver to handle 'slightly nonlinear' systems

--- solver.py
+++ solver.py
@@ -1,111 +1,148 @@
 """
-A linear constraint requires a linear combination of variables to = 0.
-We represent the constraint as a linear expression, with the '=0' implicit.
+Constraints are equations between expressions. We represent one as
+an expression, with '=0' implicit. We try to reduce each expression to
+a linear combination of variables plus a constant, then eliminate one
+of the variables, and continue.
 """
 
 from __future__ import division
 import sys
 
+from structs import Struct
+
 def make_constant(value):
-    return LinExp(value, ())
+    return Combo({const_term: value})
 
 def make_variable(name):
-    return LinExp(0, [(Variable(name), 1)])
+    return Combo({Variable(name): 1})
 
 def equate(expr1, expr2):
     return expr1 - expr2
 
-def solve(eqns):
-    for variable, value in solve_equations(eqns).items():
-        variable.assign(value)
+def solve(equations):
+    consistent_so_far = True
+    while equations:
+        pending = []
+        for expr in equations:
+            try:
+                combo = expr.evaluate()
+            except Nonlinear:
+                pending.append(expr)
+            else:
+                if combo.varies():
+                    combo.pivot() # XXX name?
+                elif zeroish(combo.constant()):
+                    pass # A consistent but useless equation: drop it.
+                else:
+                    consistent_so_far = False
+                    print 'Inconsistent', combo
+        if pending == equations:
+            return False
+        equations = pending
+    return consistent_so_far # N.B. might still be underconstrained
+
+class Nonlinear(Exception):
+    "An expression did not evaluate this time to a linear combo."
+
+class Expr(object):
+    def evaluate(self):
+        abstract
+    def get_value(self):
+        try:
+            combo = self.evaluate()
+        except Nonlinear:
+            raise NotFixed(self)
+        return combo.constant()
+    def __add__(self, other):     return Combine(self, 1, other)
+    def __sub__(self, other):     return Combine(self, -1, other)
+    def __mul__(self, other):     return Mul(self, other)
+    def __truediv__(self, other): return Div(self, other)
+
+class Combine(Struct('arg1 coeff arg2', supertype=(Expr,))):
+    def evaluate(self):
+        return self.arg1.evaluate().combine(self.coeff,
+                                            self.arg2.evaluate())
+
+class Mul(Struct('arg1 arg2', supertype=(Expr,))):
+    def evaluate(self):
+        combo1 = self.arg1.evaluate()
+        combo2 = self.arg2.evaluate()
+        if combo1.varies() and combo2.varies():
+            raise Nonlinear()
+        if combo1.varies(): return combo1.scale(combo2.constant())
+        else:               return combo2.scale(combo1.constant())
+
+class Div(Struct('arg1 arg2', supertype=(Expr,))):
+    def evaluate(self):
+        combo1 = self.arg1.evaluate()
+        combo2 = self.arg2.evaluate()
+        if combo2.varies():
+            raise Nonlinear()
+        return combo1.scale(1 / combo2.constant())
+
+class Combo(Expr):
+    def __init__(self, terms):
+        self.terms = {v: c for v,c in terms.iteritems() if not zeroish(c)}
+    def evaluate(self):
+        return self
+    def varies(self): # XXX this varies()/constant()/pivot() business duplicates work
+        terms = self.scale(1).terms
+        return any(v is not const_term for v in terms)
+    def constant(self):
+        terms = self.scale(1).terms
+        if any(v is not const_term for v in terms):
+            raise NotFixed(self)
+        return terms.get(const_term, 0)
+    def pivot(self):
+        terms = self.scale(1).terms
+        assert any(v is not const_term for v in terms)
+        ve, ce = max(((vi, ci)
+                      for vi,ci in terms.iteritems()
+                      if vi is not const_term),
+                     key=lambda (vi, ci): abs(ci))
+        ve.become(Combo({v: -c/ce
+                         for v,c in terms.iteritems()
+                         if v is not ve}))
+        # XXX Combo() does an unnecessary sweep of its argument
+    def scale(self, c):
+        accum = {}
+        self.add_into(accum, c)
+        return Combo(accum)
+    def combine(self, c, other):
+        accum = {}
+        self.add_into(accum, 1)
+        other.add_into(accum, c)
+        return Combo(accum)
+    def add_into(self, accum, c):
+        for v, coeff in self.terms.iteritems():
+            v.add_into(accum, c * coeff)
+
+class NotFixed(Exception):
+    "An expression could not be reduced to a constant value."
 
 class Variable(object):
     def __init__(self, name):
         self.name = name
-        self.value = None
+        self.combo = None
+    def become(self, combo):
+        assert self.combo is None
+        self.combo = combo
+    def add_into(self, accum, c):
+        if self.combo is None:
+            accum[self] = accum.get(self, 0) + c
+        else:
+            self.combo.add_into(accum, c)
     def get_value(self):
-        assert self.value is not None, "Unsolved: %r" % self
-        return self.value
-    def assign(self, value):
-        assert self.value is None or self.value == value
-        self.value = value
+        try:
+            if self.combo is None: raise NotFixed(self)
+            return self.combo.constant()
+        except NotFixed:
+            print 'get_value of unfixed variable', self
+            return 1            # (arbitrary)
     def __repr__(self):
         return self.name
 
-class LinExp(object):
-    "A linear expression in some variables."
-    def __init__(self, constant, terms):
-        self.constant = constant
-        self.terms = {var: val for var, val in terms if val != 0} # XXX use zeroish?
-
-    def get_value(self):
-        return self.constant + sum(c * v.get_value()
-                                   for v,c in self.terms.items())
-    def as_scalar(self):
-        assert self.is_constant(), 'nope %r' % self
-        return self.constant
-    def __add__(self, other):     return self.combine(1, other, 1)
-    def __sub__(self, other):     return self.combine(1, other, -1)
-    def __truediv__(self, other): return self.scale(1 / other.as_scalar())
-    def __mul__(self, other):
-        if self.is_constant():
-            return other.scale(self.constant)
-        else:
-            return self.scale(other.as_scalar())
-
-    def combine(self, c, e2, c2):
-        return LinExp(c * self.constant + c2 * e2.constant,
-                      ((var, (c * self.coefficient(var)
-                              + c2 * e2.coefficient(var)))
-                       for var in self.variables() | e2.variables()))
-    def scale(self, c):
-        return self.combine(c, zero, 0)
-    def coefficient(self, variable):
-        return self.terms.get(variable, 0)
-    def variables(self):
-        return set(self.terms.iterkeys())
-    def a_variable(self):
-        return next(self.terms.iterkeys(), None)
-    def is_constant(self):
-        return not self.terms
-    # The remaining methods treat LinExps as equations implicitly ==
-    # 0. So one is inconsistent if it's something like 5==0, it
-    # defines a variable if it's something like 1*x-3==0 (with just
-    # one variable), and so on.
-    def is_inconsistent(self):
-        return self.is_constant() and not zeroish(self.constant)
-    def is_tautology(self):
-        return self.is_constant() and zeroish(self.constant)
-    def defines_var(self):
-        vars = self.terms.keys()
-        return (vars if len(vars) == 1 and zeroish(1 - self.coefficient(vars[0]))
-                else ())
-    def substitute_for(self, var, eq):
-        """Return an equivalent equation with var eliminated by
-        resolving against eq (which must have a term for var)."""
-        # self - (self[var]/eq[var]) * eq
-        c = -self.coefficient(var) / eq.coefficient(var)
-        return self.combine(1, eq, c)
-    def normalize(self):
-        """Return an equivalent equation with a variable's coefficient
-        rescaled to 1."""
-        var = self.a_variable()
-        return self.scale(1 / self.coefficient(var))
-    def __repr__(self):
-        items = sorted(self.terms.items())
-        if self.constant:
-            items = [('', self.constant)] + items
-        if not items: return '(0 = 0)'
-        def format((var, c)):
-            if var == '': return '%s' % c
-            if c == 1:    return '%s' % (var,)
-            if c == -1:   return '-%s' % (var,)
-            return '%s%s' % (c, var)
-        def combiner(pair):      # XXX rename
-            f = format(pair)
-            return ' - ' + f[1:] if f.startswith('-') else ' + ' + f
-        return '(0 = %s%s)' % (format(items[0]),
-                               ''.join(map(combiner, items[1:])))
+const_term = Variable('<const>') # A fake variable for a combo's constant term.
 
 def zeroish(u):
     "Is float u approximately zero?"
@@ -121,30 +158,3 @@ def zeroish(u):
 # from run to run. Let's see if the gremlins go away.
 # (Or maybe the solver's just incorrect. I should review it.)
 epsilon = sys.float_info.epsilon * 5
-
-zero = LinExp(0, ())
-
-def solve_equations(eqs):
-    """Return a dict mapping variables to values, for those variables
-    eqs constrains to a value. An eq is a LinExp implicitly equated to
-    0."""
-    consistent, eqs = reduce_equations(eqs)
-    if not consistent:
-        return {}               # or None, or what?
-    return {var: -le.constant
-            for le in eqs
-            for var in le.defines_var()}
-
-def reduce_equations(eqs):
-    """Try to reduce eqs to an equivalent system with each variable
-    defined by a single equation. The first result is False if the
-    eqs are inconsistent. The result may still be underconstrained."""
-    for i, eqi in enumerate(eqs):
-        var = eqi.a_variable()
-        if not var: continue
-        for j, eqj in enumerate(eqs):
-            if i == j: continue
-            eqs[j] = eqj.substitute_for(var, eqi)
-            if eqs[j].is_inconsistent():
-                return False, eqs
-    return True, [eq.normalize() for eq in eqs if not eq.is_tautology()]
